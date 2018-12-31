55671d4645270e84f669a459aca872e1ddd55c85
hugofloter <hugo.paigneau@etu.utc.fr>
2018-12-31 22:22:37 +0100

    adding jupyter API

--- django_cards/detector/views.py
+++ django_cards/detector/views.py
@@ -63,7 +63,7 @@ def getCardsInPhoto(request):
     url = cfg.API_URL+'/predict'
 
     r = requests.post(url, json={
-        'id_img': ''+str(id_img)+''
+        'id_img': '3307'
     })
     list_cards = r.json()
 
