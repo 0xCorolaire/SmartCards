05c586f954c3d6fbe6eeb1d621d6de3bd81a13f2
hugofloter <hugo.paigneau@etu.utc.fr>
2019-01-04 19:38:56 +0100

    allow cors

--- django_cards/django_cards/settings.py
+++ django_cards/django_cards/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'detector',
     'django_cards',
     'coinche',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
 ]
 
 ROOT_URLCONF = 'django_cards.urls'
